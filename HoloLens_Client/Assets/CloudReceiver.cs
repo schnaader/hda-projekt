75bdb3c7e952bc4a4948aae9ccc7dfbd204192e9
TheOnlyRoxion <roxion@tobiasnixdorf.de>
2017-06-01 17:28:29 +0200

    Alter Code des CloudReceivers
    Muss aber umgeschrieben werden, weil bei UWP-Apps nicht die System.Net.Sockets genutzt werden können

--- HoloLens_Client/Assets/CloudReceiver.cs
+++ HoloLens_Client/Assets/CloudReceiver.cs
@@ -1,20 +1,78 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
+using System;
+using System.Net;
+using System.Net.Sockets;
 
-public class CloudReceiver : MonoBehaviour {
+public class CloudReceiver : MonoBehaviour
+{
 
+    private Socket _clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+    private byte[] _recieveBuffer = new byte[32 * 1024 * 1024];
+    private Mesh mesh;
     private Text textInGui;
 
+    private void SetupClient()
+    {
+        try
+        {
+            _clientSocket.Connect(new IPEndPoint(IPAddress.Loopback, 6670));
+        }
+        catch (SocketException ex)
+        {
+            Debug.Log(ex.Message);
+            textInGui.text = ex.Message;
+        }
+
+        _clientSocket.BeginReceive(_recieveBuffer, 0, _recieveBuffer.Length, SocketFlags.None, new AsyncCallback(ReceiveCallback), null);
+
+    }
+
+    private void ReceiveCallback(IAsyncResult AR)
+    {
+        //Check how much bytes are recieved and call EndRecieve to finalize handshake
+        int received = _clientSocket.EndReceive(AR);
+
+        if (received <= 0)
+            return;
+
+        //Copy the recieved data into new buffer , to avoid null bytes
+        byte[] recData = new byte[received];
+        Buffer.BlockCopy(_recieveBuffer, 0, recData, 0, received);
+
+        //Process data here the way you want , all your bytes will be stored in recData
+        var receivedString = System.Text.Encoding.Default.GetString(recData);
+        textInGui.text = "Empfangener Text: " + receivedString;
+
+        //Start receiving again
+        _clientSocket.BeginReceive(_recieveBuffer, 0, _recieveBuffer.Length, SocketFlags.None, new AsyncCallback(ReceiveCallback), null);
+    }
+
+    private ushort ReadUInt16(byte[] data, ref int index)
+    {
+        ushort result = data[index];
+        result += (ushort)(data[index + 1] << 8);
+
+        index += 2;
+
+        return result;
+    }
+
     // Use this for initialization
-    void Start () {
+    void Start()
+    {
+
+        mesh = new Mesh();
+        GetComponent<MeshFilter>().mesh = mesh;
+
         textInGui = GameObject.FindObjectOfType<Text>();
+
+        SetupClient();
     }
 
     // Update is called once per frame
-    void Update () {
-		
-	}
+    void Update()
+    {
+
+    }
 }
