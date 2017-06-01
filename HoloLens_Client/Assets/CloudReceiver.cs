f178303bf0308c6d5b9b92464690dc167e77e475
TheOnlyRoxion <roxion@tobiasnixdorf.de>
2017-06-01 17:21:49 +0200

    Code, um auf den angezeigten Text zuzugreifen

---
+++ HoloLens_Client/Assets/CloudReceiver.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CloudReceiver : MonoBehaviour {
+
+    private Text textInGui;
+
+    // Use this for initialization
+    void Start () {
+        textInGui = GameObject.FindObjectOfType<Text>();
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
