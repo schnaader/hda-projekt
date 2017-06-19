0d503e39c05f825eb6ee38c25ca2f1bbc75cc53f
Christian Schneider <schnaader@gmx.de>
2017-06-19 17:01:35 +0200

    Zusammenführen Serverskript und Mesh-Update
    - So können später Senden und Aktualisieren des Meshs besser synchronisiert werden

--- PC_Server/Assets/Scripts/DepthSourceView.cs
+++ PC_Server/Assets/Scripts/DepthSourceView.cs
@@ -1,6 +1,11 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
+using System;
+using System.Net;
+using System.Net.Sockets;
 using Windows.Kinect;
+using System.Text;
 
 public enum DepthViewMode
 {
@@ -23,7 +28,6 @@ public class DepthSourceView : MonoBehaviour
     private Vector2[] _UV;
     private int[] _Triangles;
     
-    // Only works at 4 right now
     private const int _DownsampleSize = 2;
     private const double _DepthScale = 0.1f;
     private const int _Speed = 50;
@@ -32,8 +36,18 @@ public class DepthSourceView : MonoBehaviour
     private ColorSourceManager _ColorManager;
     private DepthSourceManager _DepthManager;
 
+    private byte[] _recieveBuffer = new byte[32 * 1024 * 1024];
+    private Text textInGui;
+    private int messageCount = 0;
+
+    public Socket m_socListener, m_socWorker = null;
+
     void Start()
     {
+        textInGui = GameObject.FindObjectOfType<Text>();
+
+        StartListening();
+
         _Sensor = KinectSensor.GetDefault();
         if (_Sensor != null)
         {
@@ -98,6 +112,29 @@ public class DepthSourceView : MonoBehaviour
         GUI.BeginGroup(new Rect(0, 0, Screen.width, Screen.height));
         GUI.TextField(new Rect(Screen.width - 250 , 10, 250, 20), "DepthMode: " + ViewMode.ToString());
         GUI.EndGroup();
+    }
+
+    public void StartListening()
+    {
+        try
+        {
+            //create the listening socket...
+            m_socListener = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPEndPoint ipLocal = new IPEndPoint(IPAddress.Any, 6670);
+            //IPEndPoint ipLocal = new IPEndPoint(IPAddress.Loopback, 6670);
+            //bind to local IP Address...
+            m_socListener.Bind(ipLocal);
+            //start listening...
+            m_socListener.Listen(4);
+            // create the call back for any client connections...
+            m_socListener.BeginAccept(new AsyncCallback(OnClientConnect), null);
+            textInGui.text = String.Format("Listening on {0}...", ipLocal);
+        }
+        catch (SocketException ex)
+        {
+            Debug.Log(ex.Message);
+            textInGui.text = ex.Message;
+        }
     }
 
     void Update()
@@ -235,6 +272,68 @@ public class DepthSourceView : MonoBehaviour
         }
 
         return sum / (_DownsampleSize * _DownsampleSize);
+    }
+
+    byte[] m_DataBuffer = new byte[32 * 1024 * 1024];
+
+    public void OnClientConnect(IAsyncResult asyn)
+    {
+        try
+        {
+            textInGui.text = "Client is connecting...";
+
+            m_socWorker = m_socListener.EndAccept(asyn);
+            // now start to listen for any data...
+            m_socWorker.BeginReceive(m_DataBuffer, 0, m_DataBuffer.Length, SocketFlags.None, new AsyncCallback(ReceiveCallback), null);
+        }
+        catch (ObjectDisposedException)
+        {
+            Debug.Log("OnClientConnection: Socket has been closed\n");
+        }
+        catch (SocketException ex)
+        {
+            Debug.Log(ex.Message);
+        }
+    }
+
+    public Socket m_socClient;
+
+    public void ReceiveCallback(IAsyncResult asyn)
+    {
+        //Check how much bytes are recieved and call EndRecieve to finalize handshake
+        int received = m_socWorker.EndReceive(asyn);
+
+        if (received <= 0)
+            return;
+
+        //Copy the recieved data into new buffer , to avoid null bytes
+        byte[] recData = new byte[received];
+        Buffer.BlockCopy(_recieveBuffer, 0, recData, 0, received);
+
+        //Process data here the way you want , all your bytes will be stored in recData
+        var receivedString = System.Text.Encoding.Default.GetString(recData);
+        textInGui.text = "Ready signal received from client, sending test data #" + messageCount + "...";
+
+        // Testdaten schicken
+        messageCount++;
+        SendLineToClient("Hello client #" + messageCount);
+
+        //Start receiving again
+        m_socWorker.BeginReceive(_recieveBuffer, 0, _recieveBuffer.Length, SocketFlags.None, new AsyncCallback(ReceiveCallback), null);
+    }
+
+    private void SendLineToClient(string line)
+    {
+        SendDataToClient(Encoding.ASCII.GetBytes(line + "\n"));
+    }
+
+    private void SendDataToClient(byte[] data)
+    {
+        if (m_socWorker == null) return;
+
+        SocketAsyncEventArgs socketAsyncData = new SocketAsyncEventArgs();
+        socketAsyncData.SetBuffer(data, 0, data.Length);
+        m_socWorker.SendAsync(socketAsyncData);
     }
 
     void OnApplicationQuit()
