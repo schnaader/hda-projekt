9b329aa2d87a09d7ab3e123040f36d6a8cf605e3
Christian Schneider <schnaader@gmx.de>
2017-06-21 17:34:20 +0200

    Vorbereitungen für Anzeige des Meshes auf dem Client

--- HoloLens_Client/Assets/CloudReceiver.cs
+++ HoloLens_Client/Assets/CloudReceiver.cs
@@ -26,7 +26,11 @@ public class TaskResult
 
 public class CloudReceiver : MonoBehaviour
 {
-    //private Mesh mesh;
+    private Mesh _Mesh;
+    private Vector3[] _Vertices;
+    private Vector2[] _UV;
+    private int[] _Triangles;
+
     private Text textInGui;
     private int readyCount = 0;
 
@@ -39,9 +43,7 @@ public class CloudReceiver : MonoBehaviour
     // Use this for initialization
     void Start()
     {
-
-        //mesh = new Mesh();
-        //GetComponent<MeshFilter>().mesh = mesh;
+        CreateMesh(256, 212);
 
         textInGui = GameObject.FindObjectOfType<Text>();
 
@@ -52,6 +54,49 @@ public class CloudReceiver : MonoBehaviour
         if (!result.succeeded) return;
 
         Task.Run(StartAsyncReceiveLoop);
+    }
+
+    void CreateMesh(int width, int height)
+    {
+        _Mesh = new Mesh();
+        GetComponentInChildren<MeshFilter>().mesh = _Mesh;
+
+        _Vertices = new Vector3[width * height];
+        _UV = new Vector2[width * height];
+        _Triangles = new int[6 * ((width - 1) * (height - 1))];
+
+        int triangleIndex = 0;
+        for (int y = 0; y < height; y++)
+        {
+            for (int x = 0; x < width; x++)
+            {
+                int index = (y * width) + x;
+
+                _Vertices[index] = new Vector3(x, -y, 0);
+                _UV[index] = new Vector2(((float)x / (float)width), ((float)y / (float)height));
+
+                // Skip the last row/col
+                if (x != (width - 1) && y != (height - 1))
+                {
+                    int topLeft = index;
+                    int topRight = topLeft + 1;
+                    int bottomLeft = topLeft + width;
+                    int bottomRight = bottomLeft + 1;
+
+                    _Triangles[triangleIndex++] = topLeft;
+                    _Triangles[triangleIndex++] = topRight;
+                    _Triangles[triangleIndex++] = bottomLeft;
+                    _Triangles[triangleIndex++] = bottomLeft;
+                    _Triangles[triangleIndex++] = topRight;
+                    _Triangles[triangleIndex++] = bottomRight;
+                }
+            }
+        }
+
+        _Mesh.vertices = _Vertices;
+        _Mesh.uv = _UV;
+        _Mesh.triangles = _Triangles;
+        _Mesh.RecalculateNormals();
     }
 
     private void Update()
